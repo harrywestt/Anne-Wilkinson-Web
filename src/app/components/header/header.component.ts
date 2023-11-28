acbaff19ff8ea8a22407e360e4036c783c12622a
Harry West <80953801+harrywestt@users.noreply.github.com>
2023-11-28 00:06:41 +0000

    Updated header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,41 +7,45 @@ import { CommonModule } from '@angular/common';
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-  items: any;
-  constructor() {
-    this.items = [
-      {
-        label: 'Shop',
-        command: () => {
-          console.log('Shop');
-          // this.router.navigate(["./map"])
-        },
+  selectedItem: any;
+  items = [
+    {
+      label: 'shop',
+      command: () => {
+        console.log('Shop');
+        // this.router.navigate(["./map"])
       },
-      {
-        label: 'Mother Of The Bride',
-        command: () => {
-          console.log('MOTB');
-          // this.router.navigate(["./map"])
-        },
+    },
+    {
+      label: 'mother of the bride',
+      command: () => {
+        console.log('MOTB');
+        // this.router.navigate(["./map"])
       },
-      {
-        label: 'About',
-        command: () => {
-          console.log('About');
-          // this.router.navigate(["./map"])
-        },
+    },
+    {
+      label: 'about',
+      command: () => {
+        console.log('About');
+        // this.router.navigate(["./map"])
       },
-      {
-        label: 'Contact',
-        command: () => {
-          console.log('Contact');
-          // this.router.navigate(["./map"])
-        },
+    },
+    {
+      label: 'contact',
+      command: () => {
+        console.log('Contact');
+        // this.router.navigate(["./map"])
       },
-    ];
+    },
+  ];
+
+  selectTab(item: any): void {
+    this.selectedItem = item;
+    item.command();
   }
 
   handleLogoClick() {
     console.log('Home');
+    this.selectedItem = undefined;
   }
 }
